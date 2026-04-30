bd12999f5622dd6fe31a37f398dd8a0b617a0e46
Leah Sonowal <sonowalleah@gmail.com>
2026-04-30 11:59:44 +0530

    Add files via upload

---
+++ backend/utils/auth_utils.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from flask import request, jsonify
+
+def role_required(allowed_roles):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            role = request.headers.get('role')
+            if role not in allowed_roles:
+                return jsonify({"error": "Unauthorized access"}), 403
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
