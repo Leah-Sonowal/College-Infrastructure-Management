8b3a38ed3ae7fae7298bb5ea27cf264eb7e6e862
Leah Sonowal <sonowalleah@gmail.com>
2026-04-30 11:54:42 +0530

    Add files via upload

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'yourpassword'   # ← change this to your MySQL password
+MYSQL_DB = 'campusinfra_db'       # fixed: matches the DB name in database.sql
